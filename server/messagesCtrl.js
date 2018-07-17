7574fe87e4fa54730a57cbce3fb56ab5e362e3bf
Josiah Maina <josiah5115@gmail.com>
2018-07-17 12:27:08 -0600

    mesg cntrler capturing new messg and push to histry array in session

--- server/messagesCtrl.js
+++ server/messagesCtrl.js
@@ -4,12 +4,22 @@ module.exports = {
   getAllMessages: (req, res) => {
     res.status(200).send(allMessages);
   },
+
   createMessage: (req, res) => {
     let newMessage = {
       username: req.body.username,
       message: req.body.message
     };
+
     allMessages.push(newMessage);
+
+    if (req.session.history) {
+      req.session.history.push(newMessage);
+    } else {
+      req.session.history = [];
+      req.session.history.push(newMessage);
+    }
+    
     res.status(200).send(allMessages);
   }
 };
